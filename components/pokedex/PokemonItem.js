6e23f92b760e06fe14c0bb7c4c8201832449b5bc
John Elmo Johnson <elmoj6@gmail.com>
2022-12-05 19:46:57 +1100

    Update: PokemonItem UI and Animation

--- components/pokedex/PokemonItem.js
+++ components/pokedex/PokemonItem.js
@@ -6,30 +6,37 @@ const PokemonItem = ({ entry_number, name }) => {
   const { pokemon } = usePokemonByName(name);
   return (
     <motion.div
-    initial={{opacity: 0}}
-    whileInView={{opacity: 1}}
-    className="bg-white shadow rounded overflow-hidden h-20 flex items-center">
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 1 }}
+      className="bg-white shadow rounded overflow-hidden h-20 flex items-center hover:cursor-pointer hover:shadow-xl"
+    >
       <AnimatePresence>
         {pokemon.defaultSprite ? (
           <motion.img
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{delay: 0.5}}
+            transition={{ delay: 0.5 }}
             src={pokemon.defaultSprite}
             alt={name}
             className="h-20 w-20"
           />
         ) : (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            className="bg-neutral-200 h-20 w-20 animate-pulse"
-          />
+          <div className="h-20 w-20 p-4 flex items-center justify-center">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="bg-neutral-200 rounded-full h-12 w-12 animate-pulse"
+            />
+          </div>
         )}
       </AnimatePresence>
-      <div className="leading-none flex flex-col space-y-0 px-3">
-        <span className="font-semibold text-xs text-neutral-400">No. {String(entry_number).padStart(3, "0")}</span>
+      <div className="leading-none flex flex-col space-y-0 px-3 w-full mr-3">
+        <span className="font-semibold text-xs text-neutral-400">
+          No. {String(entry_number).padStart(3, "0")}
+        </span>
         <h1 className="font-bold text-lg capitalize">{name}</h1>
       </div>
     </motion.div>
