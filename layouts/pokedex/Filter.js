a5473fa9a8e507973cf84805531413d6c363fba6
John Elmo Johnson <elmoj6@gmail.com>
2022-12-12 18:45:32 +1100

    Removed Filter By Pokedex Entry text

--- layouts/pokedex/Filter.js
+++ layouts/pokedex/Filter.js
@@ -4,7 +4,6 @@ import DexSelect from "../../components/pokedex/DexSelect";
 const Filter = () => {
   return (
     <div className="flex flex-col space-y-3">
-      <h5 className="font-semibold text-muted">Filter by Pokedex Entry</h5>
       <DexSelect />
     </div>
   );
